6e949ed510214cfb5a8f4316363567b1378f2d6d
Adin <liutaadin@yahoo.com>
2023-04-24 09:48:31 +0300

    Add API Key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '9a765c0a-1cbe-43e9-bbde-6070ecca12b9';
 export const MODAL_CLOSE_SEC = 2.5;
